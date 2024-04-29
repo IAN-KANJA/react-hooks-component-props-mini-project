601d65e03b9c838f72b75c1d9270205615ad16b5
iannjau <lantanacreations@gmail.com>
2024-04-29 08:52:43 +0300

    add my file

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Header from "./Header";
 import blogData from "../data/blog";
 
 console.log(blogData);
